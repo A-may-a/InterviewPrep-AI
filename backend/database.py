fb1c656ed7eb1a393dcf707f3fa551ee7dfa51e2
A-may-a <amayajayan2005@gmail.com>
2026-06-17 13:02:31 +0530

    Added accidently removed line

--- backend/database.py
+++ backend/database.py
@@ -16,7 +16,6 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "interview_prep_db")
 DB_PORT = os.getenv("DB_PORT", "3306")
 
-import os
 
 DATABASE_URL = (
     f"mysql+pymysql://"
@@ -27,6 +26,7 @@ DATABASE_URL = (
     f"{os.getenv('MYSQL_DATABASE')}"
 )
 
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
